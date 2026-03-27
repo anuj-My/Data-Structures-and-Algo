19cf8c59c63c36d74ce77cd7341d3b3747082907
Anuj <anuj89806@gmail.com>
2026-03-27 17:29:53 +0530

    frequency counter - anagram

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -57,3 +57,35 @@ function same1(arr1, arr2) {
 console.log(same1([1, 2, 3], [4, 1, 9])); // true
 console.log(same1([1, 2, 3], [1, 9])); // false
 console.log(same1([1, 2, 1], [4, 4, 1])); // false
+
+// ? Frequency Counter - validAnagram
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+console.log("------anagram-------");
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  let result = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    if (result[str1[i]]) {
+      result[str1[i]] += 1;
+    } else {
+      result[str1[i]] = 1;
+    }
+  }
+
+  for (const str of str2) {
+    if (!result[str]) return false;
+    result[str] -= 1;
+  }
+
+  return true;
+}
+
+console.log(validAnagram("", "")); // true
+console.log(validAnagram("aaz", "zza")); // false
+console.log(validAnagram("anagram", "nagaram")); // true
+console.log(validAnagram("rat", "car")); // false) // false
+console.log(validAnagram("awesome", "awesom")); // false
+console.log(validAnagram("amanaplanacanalpanama", "acanalmanplanpamana")); // false
+console.log(validAnagram("qwerty", "qeywrt")); // true
+console.log(validAnagram("texttwisttime", "timetwisttext")); // true
