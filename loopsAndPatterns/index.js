6ebf764790afde2c5ab1d4082af64fb41f33db87
Anuj <anuj89806@gmail.com>
2026-04-02 21:50:43 +0530

    pyramid and inverted right angle star pattern problem

--- loopsAndPatterns/index.js
+++ loopsAndPatterns/index.js
@@ -10,3 +10,37 @@ function RightAngleStarPattern(rows) {
 }
 
 RightAngleStarPattern(5);
+
+console.log("reverse Right angle triangle star pattern...");
+
+function reverseRightAngleStar(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let str = "";
+
+    for (let j = 1; j <= rows + 1 - i; j++) {
+      str = str + "*";
+    }
+    console.log(str);
+  }
+}
+
+reverseRightAngleStar(5);
+
+console.log("pyramid star pattern...");
+
+function pyramid(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let str = "";
+
+    for (let j = 1; j <= rows - i; j++) {
+      str = str + " ";
+    }
+
+    for (let j = 1; j <= 2 * i - 1; j++) {
+      str = str + "*";
+    }
+    console.log(str);
+  }
+}
+
+pyramid(5);
